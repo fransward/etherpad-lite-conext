87632dfd4dea1b906c36f4b8e25f2fbbcb9ee91b
mdobrinic@cozmanova <mdobrinic@cozmanova@96a3c335-62f2-4a8d-be43-dd49c1b6f1f9>
2011-11-22 10:08:14 +0000

    fix
    git-svn-id: https://svn.surfnet.nl/svn/coin-private/etherpad-lite-conext/trunk@183 96a3c335-62f2-4a8d-be43-dd49c1b6f1f9

--- eplconext/lib/GroupRel/Model/Message.php
+++ eplconext/lib/GroupRel/Model/Message.php
@@ -1,19 +1,42 @@
 <?php
-class Message {
-	
-	public $_subject;
-	public $_content;
-	
-	public $_sender;
-	public $_recipients = array();
+/**
+ * Model class that represents a Message
+ * 
+ * @author dopey (mdobrinic@cozmanova.com)
+ * for SURFnet (www.surfnet.nl)
+ *
+ */
 
-	public function addRecipient($recipient) {
-		if (is_array($this->_recipients)) {
-			$this->_recipients[] = $recipient;
-		} else {
-			$this->_recipients = array($recipient);
-		}
-	} 
-	
+class Message {
+  
+  public $_subject;
+  public $_content;
+  
+  public $_sender;
+  public $_recipients = array();
+
+  public function addRecipient($recipient) {
+    if (is_array($this->_recipients)) {
+      $this->_recipients[] = $recipient;
+    } else {
+      $this->_recipients = array($recipient);
+    }
+  }
+
+  public function __toString() {
+    if (count($this->_recipients) > 0) {
+      $r = $this->_recipients[0]; 
+    } else {
+      $r = "[[undefined]]";
+    }
+    return <<<HERE
+From: {$this->_sender}
+To: {$r}
+Subject: {$this->_subject}
+{$this->_content}   
+HERE
+;
+  }
+  
 }
 ?>
