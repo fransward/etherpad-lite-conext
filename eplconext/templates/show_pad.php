22543ee998b5f810371d453f782d9d8b70569cde
mdobrinic@cozmanova <mdobrinic@cozmanova@96a3c335-62f2-4a8d-be43-dd49c1b6f1f9>
2011-11-22 10:08:42 +0000

    little layout fix
    git-svn-id: https://svn.surfnet.nl/svn/coin-private/etherpad-lite-conext/trunk@185 96a3c335-62f2-4a8d-be43-dd49c1b6f1f9

--- eplconext/templates/show_pad.php
+++ eplconext/templates/show_pad.php
@@ -1,12 +1,12 @@
 <?php
 // check precondition
-global $userdata, $padurl;
+global $userdata, $padurl, $padname;
 $proceed = isset($userdata) &&
 		isset($padurl);		// Web_CGIUtil::get_self_url()
 
 if (! $proceed ) exit();
 ?>
 <?php
-print "<div style=\"align:center\"><h1>{$padname}</h1></div>";
+print "<div style=\"align:center\"><h1 style=\"font-family : Arial; font-size: medium;\">{$padname}</h1></div>";
 print "<iframe src=\"{$padurl}\" width=\"100%\" height=\"100%\"></iframe>";
 ?>
